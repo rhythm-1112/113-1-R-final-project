d9808d5c65e70c2aa134f8c38f916b4fa56e0de2
econmtlin <econ.mtlin@gmail.com>
2024-11-20 05:49:19 +0000

    11/20

--- practice.R
+++ practice.R
@@ -61,4 +61,12 @@ exchangeRate <- exchangeRate %>%
   select(-year)                                          # Remove the temporary year column
 glimpse(exchangeRate$PERIOD)
 
-#thank you
+#6
+survey <- read_csv("https://raw.githubusercontent.com/tpemartin/113-1-R/refs/heads/main/Lecture-notes/ee-survey.csv") 
+
+glimpse(survey) 
+survey <- survey %>%
+  mutate(Timestamp = mdy_hms(Timestamp, tz = "Asia/Taipei"))
+glimpse(survey) 
+
+#tz
