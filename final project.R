91d3e16c87b307bc2cb836765373ca5178235243
econmtlin <econ.mtlin@gmail.com>
2024-11-27 06:11:46 +0000

    :)

--- final project.R
+++ final project.R
@@ -7,3 +7,9 @@ OpenData <- OpenData %>%
   mutate(year = as.Date(paste0(year, "-01-01"), format = "%Y-%m-%d"))
 
 glimpse(OpenData)
+
+ggplot(data = OpenData)
+ggplot(
+  data = OpenData,
+  mapping = aes(x = population_thousands, y = labor_force_thousands)
+)
